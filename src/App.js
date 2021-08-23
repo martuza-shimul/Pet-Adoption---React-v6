052573bda44db7f354ff3c783c11b0d42ca62402
Martuza Shimul <martuza.shimul@gmail.com>
2021-08-24 05:50:34 +0600

    js tooling added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
-      name: "Luna",
+      name: "Luna P",
       animal: "Dog",
       breed: "Havanese",
     }),
