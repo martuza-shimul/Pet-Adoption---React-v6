b8574bb8b9441fa74ef16b7deefb6cfb908f1679
Martuza Shimul <martuza.shimul@gmail.com>
2021-08-26 12:07:05 +0600

    Details page added with Class component

--- src/Pet.js
+++ src/Pet.js
@@ -1,4 +1,4 @@
-// import React from "react";
+import { Link } from "react-router-dom";
 
 const Pet = ({ name, animal, breed, images, location, id }) => {
   let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
@@ -6,7 +6,7 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
     hero = images[0];
   }
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -14,7 +14,7 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`} </h2>
       </div>
-    </a>
+    </Link>
   );
 };
 
